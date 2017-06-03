d3db2ba4ff97ebe695109a0d15df471a8c1b42fb
ojones <wakeupoj@gmail.com>
2017-06-03 06:25:36 -0700

    styling

--- workshop-algorithm/src/components/step.js
+++ workshop-algorithm/src/components/step.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
 
 class Step extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      steps: [
+        'step 1',
+        'step 2',
+        'step 3',
+        'step 4',
+        'step 5',
+        'step 6'
+      ],
+      index: 0,
+      button: 'next'
+   };
+  }
+
   render() {
-      return <div>hi</div>;
+      return (
+        <div>
+          <div className="step-text">{this.state.steps[this.state.index]}</div>
+          <Button
+            bsStyle="success"
+            bsSize="large"
+            onClick={event => this.setState({ index: this.state.index + 1 })}>
+            { this.state.button }
+          </Button>
+        </div>
+      );
   }
 }
 
--- workshop-algorithm/src/index.js
+++ workshop-algorithm/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
